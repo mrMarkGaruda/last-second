acc4516ee183625eda4519be9ecb60483db43a67
Mark Abramenko <mark.abramenko@epita.fr>
2024-09-24 14:54:58 +0200

    first commit

---
+++ src/main/java/com/bagdouri/lastsecond/LastsecondApplication.java
@@ -0,0 +1,13 @@
+package com.bagdouri.lastsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastsecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastsecondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bagdouri/lastsecond/LastsecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagdouri.lastsecond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastsecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
