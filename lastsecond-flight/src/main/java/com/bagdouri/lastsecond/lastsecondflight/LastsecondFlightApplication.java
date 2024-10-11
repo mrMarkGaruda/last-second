5fe429788dc9cfb7159dde1f253681878f25d415
Mark Abramenko <mark.abramenko@epita.fr>
2024-10-11 16:43:19 +0200

    Started database

---
+++ lastsecond-flight/src/main/java/com/bagdouri/lastsecond/lastsecondflight/LastsecondFlightApplication.java
@@ -0,0 +1,13 @@
+package com.bagdouri.lastsecond.lastsecondflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastsecondFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastsecondFlightApplication.class, args);
+	}
+
+}
